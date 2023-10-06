a1b08c5d2ff68062cfa8e28f651a8d0063e6be00
Eteims <8teims@gmail.com>
2023-10-06 01:30:28 +0100

    Code update

--- modules/linear.js
+++ modules/linear.js
@@ -1,28 +1,14 @@
 // A vector class it takes in context, x, y and color
 
+class Matrix {
+    constructor()
+}
+
 export class Vector{
-    constructor(context, x, y, color){
-        this.context = context;
+    constructor( x, y, color){
         this.x = x;
         this.y = y;
         this.color = color;  
-    }
-
-    // https://stackoverflow.com/a/6333775
-    draw(){
-        let headlen = 10; // length of head in pixels
-        let dx = this.x - 0;
-        let dy = this.y - 0;
-        let angle = Math.atan2(dy, dx);
-        this.context.beginPath();
-        this.context.moveTo(0, 0);
-        this.context.lineTo(this.x, this.y);
-        this.context.lineTo(this.x - headlen * Math.cos(angle - Math.PI / 6), this.y - headlen * Math.sin(angle - Math.PI / 6));
-        this.context.moveTo(this.x, this.y);
-        this.context.lineTo(this.x - headlen * Math.cos(angle + Math.PI / 6), this.y - headlen * Math.sin(angle + Math.PI / 6));
-        this.context.lineWidth = 3
-        this.context.strokeStyle = this.color
-        this.context.stroke();
     }
 
     add(other){
@@ -33,3 +19,86 @@ export class Vector{
         return new Vector(this.context, x, y, color);
     }
 };
+
+export class VectorSpace {
+    // A vector space contains vectors
+    // The vector space draws the vectors and the grid with the render function
+    constructor(canvas, context) {
+        this.canvas = canvas;
+        this.ctx = context;
+        this.vectors = [];
+    }
+
+    drawSpace(){
+        this.drawGrid();
+        this.addUnitVectors();
+    }
+
+    addUnitVectors() {
+        let i_hat = new Vector(50, 0, "red");
+        let j_hat = new Vector(0, 50, "green");
+
+        this.vectors.push(i_hat, j_hat);
+        console.log(i_hat);
+        this.drawVector(i_hat);
+        this.drawVector(j_hat);
+    }
+
+    addVector(vect) {
+        this.vectors.push(vect);
+        this.drawVector(vect);
+    }
+
+    // https://stackoverflow.com/a/6333775
+    drawVector(vect) {
+        let headlen = 10; // length of head in pixels
+        let dx = vect.x - 0;
+        let dy = vect.y - 0;
+        let angle = Math.atan2(dy, dx);
+        this.ctx.beginPath();
+        this.ctx.moveTo(0, 0);
+        this.ctx.lineTo(vect.x, vect.y);
+        this.ctx.lineTo(vect.x - headlen * Math.cos(angle - Math.PI / 6), vect.y - headlen * Math.sin(angle - Math.PI / 6));
+        this.ctx.moveTo(vect.x, vect.y);
+        this.ctx.lineTo(vect.x - headlen * Math.cos(angle + Math.PI / 6), vect.y - headlen * Math.sin(angle + Math.PI / 6));
+        this.ctx.lineWidth = 3
+        this.ctx.strokeStyle = vect.color
+        this.ctx.stroke();
+    }
+
+    drawGrid() {
+        // transform the canvas grid to cartesian
+        this.ctx.translate(this.canvas.width / 2, this.canvas.height / 2);
+        this.ctx.scale(1, -1);
+
+        // Draw horizontal grid lines
+        this.ctx.beginPath();
+        this.ctx.moveTo(-250, 0);
+        this.ctx.lineTo(250, 0);
+        this.ctx.lineWidth = 2;
+        this.ctx.stroke();
+
+        for (let i = -250; i <= 250; i += 50) {
+            this.ctx.beginPath();
+            this.ctx.moveTo(-250, i);
+            this.ctx.lineTo(250, i);
+            this.ctx.lineWidth = 1;
+            this.ctx.stroke();
+        }
+
+        // Draw vertical grid lines
+        this.ctx.beginPath();
+        this.ctx.moveTo(0, -250);
+        this.ctx.lineTo(0, 250);
+        this.ctx.lineWidth = 2;
+        this.ctx.stroke();
+
+        for (let i = -250; i <= 250; i += 50) {
+            this.ctx.beginPath();
+            this.ctx.moveTo(i, -250);
+            this.ctx.lineTo(i, 250);
+            this.ctx.lineWidth = 1;
+            this.ctx.stroke();
+        }
+    }
+}
--- script.js
+++ script.js
@@ -1,51 +1,30 @@
-import { Vector } from "./modules/linear.js";
+import { Vector, VectorSpace } from "./modules/linear.js";
 
 const canvas = document.getElementById("graph");
 const ctx = canvas.getContext("2d");
 
-// transform the canvas grid to cartesian
-ctx.translate( canvas.width/2, canvas.height/2);
-ctx.scale(1,-1);
+let vs = new VectorSpace(canvas, ctx);
+vs.drawSpace();
 
-// Draw horizontal grid lines
-ctx.beginPath();
-ctx.moveTo(-250, 0);
-ctx.lineTo(250, 0);
-ctx.lineWidth = 2;
-ctx.stroke();
+const new_v = new Vector(50, 100, "yellow");
+vs.addVector(new_v);
 
-for (let i = -250; i <= 250; i += 50) {
-    ctx.beginPath();
-    ctx.moveTo(-250, i);
-    ctx.lineTo(250, i);
-    ctx.lineWidth = 1;
-    ctx.stroke();
-}
+// // visualize vector addition
+// const i_hat = new Vector(ctx, 50, 0, "red");
+// i_hat.draw();
 
-// Draw vertical grid lines
-ctx.beginPath(); 
-ctx.moveTo(0, -250); 
-ctx.lineTo(0, 250);
-ctx.lineWidth = 2;
-ctx.stroke(); 
+// const j_hat = new Vector(ctx, 0, 50, "green")
+// j_hat.draw();
 
-for (let i = -250; i <= 250; i += 50) {
-    ctx.beginPath(); 
-    ctx.moveTo(i, -250); 
-    ctx.lineTo(i, 250);
-    ctx.lineWidth = 1;
-    ctx.stroke(); 
-}
+// let result = i_hat.add(j_hat);
+// result.draw();
 
-// visualize vector addition
-const x_unit = new Vector(ctx, 50, 0, "red");
-x_unit.draw();
-
-const y_unit = new Vector(ctx, 0, 50, "green")
-y_unit.draw();
-
-let result = x_unit.add(y_unit);
-result.draw();
+// Draw the circle
+// ctx.beginPath();
+// ctx.arc(0, 0, 4, 0, 2 * Math.PI);
+// ctx.fillStyle = "black";
+// ctx.fill();
+// ctx.closePath();
 
 // const angleRadians = Math.atan2(-50, 50);
 // console.log(angleRadians)
